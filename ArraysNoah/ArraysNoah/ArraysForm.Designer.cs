19d845916f85ac80c8e23ba67a9ad7c4c8401a01
Noah <noah.colbourne@ocsbstudent.ca>
2019-11-22 13:06:15 -0500

    Initial commit

---
+++ ArraysNoah/ArraysNoah/ArraysForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ArraysNoah
+{
+    partial class frmArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstAverage = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(312, 357);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(91, 26);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(338, 46);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lstAverage
+            // 
+            this.lstAverage.FormattingEnabled = true;
+            this.lstAverage.Location = new System.Drawing.Point(293, 171);
+            this.lstAverage.Name = "lstAverage";
+            this.lstAverage.Size = new System.Drawing.Size(120, 95);
+            this.lstAverage.TabIndex = 2;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(206, 357);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(50, 13);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "Average:";
+            // 
+            // frmArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstAverage);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmArrays";
+            this.Text = "Arrays by Noah";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstAverage;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
